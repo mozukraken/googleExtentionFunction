7f59a3a5779b6778cdd6722641ecef49ce17b6d6
mozukraken <mozukraken@gmail.com>
2018-02-10 23:37:34 +0900

    タブ一覧を表示.

--- 02_ShowTabsInfo/popup.js
+++ 02_ShowTabsInfo/popup.js
@@ -2,7 +2,13 @@
 
 chrome.tabs.query({}, function(tabs) {
   var i;
+  var results = document.getElementById('results');
+  var titles = [];
   for (i = 0; i < tabs.length; i++) {
-    console.log(tabs[i].title);
+    titles.push(tabs[i].title);
+    // console.log(tabs[i].title);
   }
+
+  results.value = titles.join("\n");
+  results.select();
 });
